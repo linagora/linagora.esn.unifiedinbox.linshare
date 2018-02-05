9b7da3dc95dcd3c70968f47afc070f6bc12f1df6
Sang NGO DOAN <sngodoan@linagora.com>
2018-02-05 15:44:02 +0700

    linagora.esn.admin#361 use Linshare file upload to upload attachment

--- test/unit-frontend/mocks/module.js
+++ test/unit-frontend/mocks/module.js
@@ -1,9 +1,15 @@
 'use strict';
 
 angular.module('esn.file', [])
-  .constant('DEFAULT_FILE_TYPE', 'text');
+  .constant('DEFAULT_FILE_TYPE', 'text')
+  .factory('fileUploadService', function() {
+    return {};
+  });
 angular.module('linagora.esn.unifiedinbox', []);
 angular.module('linagora.esn.linshare', [])
   .factory('esnLinshareApiClient', function() {
+    return {};
+  })
+  .factory('linshareFileUpload', function() {
     return {};
   });
