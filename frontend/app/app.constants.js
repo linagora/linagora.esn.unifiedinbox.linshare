9595c0949e0ebf83f63b5b42ab88b9e4efddac21
Dung T. Pham <dtpham@linagora.com>
2018-03-14 18:11:47 +0700

    linagora.esn.unifiedinbox.linshare#3 Fix error of LinShare attachments message does not appear when there is no email body

--- frontend/app/app.constants.js
+++ frontend/app/app.constants.js
@@ -3,5 +3,8 @@
 
   angular.module('linagora.esn.unifiedinbox.linshare')
     .constant('INBOX_LINSHARE_ATTACHMENT_TYPE', 'linshare')
-    .constant('INBOX_LINSHARE_EMAIL_ADDITIONAL_MESSAGE', 'This email contains %s Linshare attachment(s).');
+    .constant('INBOX_LINSHARE_EMAIL_ADDITIONAL_MESSAGE_TEMPLATES', {
+      plural: 'This email contains %s LinShare attachments.',
+      singular: 'This email contains a LinShare attachment.'
+    });
 })(angular);
